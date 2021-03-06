97c7d9176ca3672c843ae8e00e45ad2d31a44b3b
mikecrbenton <mikecrbenton@hotmail.com>
2021-03-06 13:54:09 -0600

    resource model and router get/post working correctly

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,24 @@
-// build your `Resource` model here
+const db = require("../../data/dbConfig")
+
+// HELPER FUNCTION FOR insertResource()
+function getResourceById(id) {
+   return db("resource")
+     .where("resource_id",id)
+     .first();
+ }
+
+function getResources(){
+   return db("resource")
+}
+
+function insertResource(newResource) {
+   return db('resource').insert(newResource)
+     .then( (returnedResources) => {
+       return getResourceById(returnedResources[0]);
+     });
+ }
+
+module.exports = {
+   getResources,
+   insertResource
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -4,4 +4,23 @@ const Resource = require("./model")
 
 const router = express.Router()
 
+router.get("/api/resources", async (req, res, next) => {
+	try {
+		const resourcesFromDB = await Resource.getResources()
+		res.json(resourcesFromDB)
+	} catch(err) {
+		next(err)
+	}
+})
+
+router.post('/api/resources', (req, res, next) => {
+   Resource.insertResource(req.body)
+      .then((data) => {
+         res.status(201).json(data)
+      })
+      .catch((error) => {
+         next(error)
+      })
+});
+
 module.exports = router
